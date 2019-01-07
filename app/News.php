25113bbe8193a9f1dc99ff0d1846bd0439412263
寺部　裕介 <yusuketerabe@terabuyuusukenoMacBook-Air.local>
2019-01-07 19:08:39 +0900

    first commit

---
+++ app/News.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    //以下が追記したもの
+    public static $rules = array(
+        'title' => 'required',
+        'body' => 'required',
+    );
+}
