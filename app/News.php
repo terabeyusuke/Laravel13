928fed5c90daaa7e98d2268afcf0c6bd84472901
寺部　裕介 <yusuketerabe@terabuyuusukenoMacBook-Air.local>
2019-01-08 19:09:04 +0900

    secand commit

--- app/News.php
+++ app/News.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
+
     //以下が追記したもの
+    protected $guarded = array('id');
     public static $rules = array(
         'title' => 'required',
         'body' => 'required',
